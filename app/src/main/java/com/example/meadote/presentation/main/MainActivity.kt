24ce89bae681678cf730d2c4386e4246b02c0bda
bastos-eduardo <ti.02@brazzo.com.br>
2020-08-27 15:49:38 -0300

    Continuação do front da tela inicial.

--- app/src/main/java/com/example/meadote/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/meadote/presentation/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.meadote.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -37,13 +38,41 @@ class MainActivity :
         nav_view.setNavigationItemSelectedListener(this)
     }
 
+    override fun onCreateOptionsMenu(
+        menu: Menu?
+    ): Boolean {
+        menuInflater.inflate(
+            R.menu.activity_main_toolbar,
+            menu
+        )
+        return true
+    }
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
+    /*  when (item.itemId) {
             R.id.nav_home -> {
                 Toast.makeText(this, "teste", Toast.LENGTH_SHORT).show()
             }
-        }
+        } */
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    override fun onOptionsItemSelected(
+        item: MenuItem
+    ): Boolean {
+        return when (item.itemId) {
+            R.id.itBusca -> {
+                Toast.makeText(
+                    this,
+                    "Em Breve",
+                    Toast.LENGTH_LONG
+                ).show()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
